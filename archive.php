839594ee3ad2263817a628aca001a02152e8e389
Christian Wach <needle@haystack.co.uk>
2018-04-28 18:47:24 +0100

    add feature images to archive listings

--- archive.php
+++ archive.php
@@ -19,29 +19,29 @@
 
 
 	<?php
-	
+
 	// Search Nav
 	$pl = get_next_posts_link('&laquo; Older Posts');
 	$nl = get_previous_posts_link('Newer Posts &raquo;');
-	
+
 	?>
-	
+
 	<?php if ( $nl != '' OR $pl != '' ) { ?>
-	
+
 	<ul class="blog_navigation clearfix">
 		<?php if ( $nl != '' ) { ?><li class="alignright"><?php echo $nl; ?></li><?php } ?>
 		<?php if ( $pl != '' ) { ?><li class="alignleft"><?php echo $pl; ?></li><?php } ?>
 	</ul>
-	
+
 	<?php } ?>
-	
-	
-	
+
+
+
 	<div class="main_column_inner">
-	
+
 		<div class="post">
 		<div class="post_header archive_header">
-			
+
 		<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
 		<?php /* If this is a category archive */ if (is_category()) { ?>
 		<h2 class="pagetitle">Archive for &#8216;<?php single_cat_title(); ?>&#8217;</h2>
@@ -58,25 +58,57 @@
 		<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
 		<h2 class="pagetitle">Blog archives</h2>
 		<?php } ?>
-		
+
 		</div>
 		</div>
-		
+
 	</div><!-- /main_column_inner -->
-	
-	
-	
+
+
+
 	<div class="main_column_inner">
-	
+
 		<?php while (have_posts()) : the_post(); ?>
 
 		<div class="post">
 
-			<div class="post_header">
-			<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-			<p class="postname"><?php the_time('F jS, Y') ?>  by <?php the_author_posts_link(); ?></p>
+			<?php
+
+			// init
+			$has_feature_image = false;
+			$feature_image_class = '';
+
+			// do we have a feature image?
+			if ( has_post_thumbnail() ) {
+				$has_feature_image = true;
+				$feature_image_class = ' has_feature_image';
+			}
+
+			?>
+
+			<div class="post_header<?php echo $feature_image_class; ?>">
+
+				<div class="post_header_inner">
+
+					<?php
+
+					// show feature image when we have one
+					if ( $has_feature_image ) {
+						echo get_the_post_thumbnail( get_the_ID(), 'medium-640' );
+					}
+
+					?>
+
+					<div class="post_header_text">
+
+						<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>"><?php the_title(); ?></a></h2>
+
+					</div><!-- /post_header_text -->
+
+				</div><!-- /post_header_inner -->
+
 			</div><!-- /post_header -->
-	
+
 			<div class="entry clearfix">
 				<?php the_content('Read the rest of this entry &raquo;'); ?>
 			</div>
@@ -92,25 +124,25 @@
 
 
 	<?php if ( $nl != '' OR $pl != '' ) { ?>
-	
+
 	<ul class="blog_navigation clearfix">
 		<?php if ( $nl != '' ) { ?><li class="alignright"><?php echo $nl; ?></li><?php } ?>
 		<?php if ( $pl != '' ) { ?><li class="alignleft"><?php echo $pl; ?></li><?php } ?>
 	</ul>
-	
+
 	<?php } ?>
-	
-	
-	
+
+
+
 <?php else : ?>
 
 
 
 	<div class="main_column_inner" id="main_column_splash">
-	
+
 		<div class="post">
 		<div class="post_header archive_header">
-			
+
 		<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
 		<?php /* If this is a category archive */ if (is_category()) { ?>
 		<h2 class="pagetitle">Nothing found for &#8216;<?php single_cat_title(); ?>&#8217;</h2>
@@ -127,32 +159,32 @@
 		<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
 		<h2 class="pagetitle">Nothing found in the Blog Archives</h2>
 		<?php } ?>
-	
+
 		</div>
 		</div>
-		
+
 	</div><!-- /main_column_inner -->
 
 
 
 	<div class="main_column_inner">
-	
+
 		<div class="post">
-	
+
 		<div class="entrytext clearfix">
-			
+
 			<p>Try searching for something?</p>
 
 			<?php include ( get_template_directory() . '/searchform.php' ); ?>
-	
+
 		</div><!-- /entrytext -->
-		
+
 		</div><!-- /post -->
-	
+
 	</div><!-- /main_column_inner -->
 
-	
-	
+
+
 <?php endif; ?>
 
 
