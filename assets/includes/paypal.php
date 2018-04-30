e64d349446c423bfba5c03a398f082e66861f6a2
Christian Wach <needle@haystack.co.uk>
2018-04-30 12:01:50 +0100

    host donate button

--- assets/includes/paypal.php
+++ assets/includes/paypal.php
@@ -18,7 +18,7 @@ NOTES
 	<form class="paypal_donate" action="https://www.paypal.com/cgi-bin/webscr" method="post" style="width: 93px; height: 32px;">
 		<input type="hidden" name="cmd" value="_s-xclick" />
 		<input type="hidden" name="hosted_button_id" value="9611686" />
-		<input type="image" src="https://www.paypal.com/en_GB/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="Donate to Spirit of Football via PayPal" />
+		<input type="image" src="<?php echo get_template_directory_uri(); ?>/assets/images/interface/donate-button.gif" border="0" name="submit" alt="Donate to Spirit of Football via PayPal" />
 	</form>
 
 </div><!-- /paypal_donate -->
