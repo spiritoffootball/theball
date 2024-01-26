a67ba672eae09a540c74f434e2e678d5a349c9b1
Christian Wach <needle@haystack.co.uk>
2024-01-26 14:39:36 +0000

    Use SOF subdomain for Brazil links

--- assets/includes/network-white.php
+++ assets/includes/network-white.php
@@ -20,7 +20,7 @@ defined( 'ABSPATH' ) || exit;
 		<h3><?php esc_html_e( 'The SOF Network', 'theball' ); ?></h3>
 
 		<ul>
-			<li class="brazil_icon"><a href="https://spiritoffootball.com.br/" title="<?php esc_attr_e( 'Spirit of Football Brazil', 'theball' ); ?>"><?php esc_html_e( 'Spirit of Football Brazil', 'theball' ); ?></a></li>
+			<li class="brazil_icon"><a href="https://br.spiritoffootball.com/" title="<?php esc_attr_e( 'Spirit of Football Brazil', 'theball' ); ?>"><?php esc_html_e( 'Spirit of Football Brazil', 'theball' ); ?></a></li>
 			<li class="cic_icon"><a href="https://spiritoffootball.com/" title="<?php esc_attr_e( 'Spirit of Football CIC', 'theball' ); ?>"><?php esc_html_e( 'Spirit of Football CIC', 'theball' ); ?></a></li>
 			<li class="sof_network_last germany_icon"><a href="https://spirit-of-football.de/" title="<?php esc_attr_e( 'Spirit of Football Germany', 'theball' ); ?>"><?php esc_html_e( 'Spirit of Football Germany', 'theball' ); ?></a></li>
 		</ul>
